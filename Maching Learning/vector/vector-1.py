bab6a4885624b6466d156da1a0c6f0cdc148ed9e
tiewangw <tiewanga@126.com>
2020-10-29 21:34:44 +0800

    创建向量vector

---
+++ Maching Learning/vector/vector-1.py
@@ -0,0 +1,12 @@
+# 创建一个向量（vector）
+
+import numpy as np
+# 创建一个行向量
+vector_row = np.array([1,2,3])
+# 创建一个列向量
+vector_rcolumn = np.array([ [1],
+                            [2],
+                            [3]])
+
+print(vector_row)
+print(vector_rcolumn)
