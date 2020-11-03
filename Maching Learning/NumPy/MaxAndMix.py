a5cbdff395b25fec0844b4f426f02cc7fc2c3996
tiewangw <tiewanga@126.com>
2020-11-03 20:26:18 +0800

    数组的一些统计值

---
+++ Maching Learning/NumPy/MaxAndMix.py
@@ -0,0 +1,19 @@
+import  numpy as np
+
+matrix = np.array([
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+])
+
+# 最大值
+print(np.max(matrix))
+
+# 最小值
+print(np.min(matrix))
+
+# 每一列的最大值
+print(np.max(matrix,axis=0))  # [7 8 9]
+
+# 每一行的最大值
+print(np.max(matrix,axis=1))  # [3 6 9]
---
+++ Maching Learning/NumPy/Mean-var-std.py
@@ -0,0 +1,21 @@
+# 计算数组的一些描述性统计值
+import  numpy as np
+
+matrix = np.array([
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+])
+
+# 返回平均值
+print(np.mean(matrix))  # 5
+
+# 返回方差
+print(np.var(matrix))  # 6.666666666666667
+
+# 返回标准差
+print(np.std(matrix))  # 2.581988897471611
+print("----------------")
+
+# 返回每一列的平均值
+print(np.mean(matrix,axis=0))  # [4. 5. 6.]
